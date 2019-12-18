4a2699cf1f9a1ac64a5233d28b9305a2b25c81c0
caseyccc <51410580+caseyccc@users.noreply.github.com>
2019-12-18 16:29:05 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,5 @@
 #Install choco .exe and add choco to PATH
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 # Install all the packages
 
 # Browsers
