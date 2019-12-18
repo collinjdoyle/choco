ccf2315b59ff44719cb6caab5b121047db6aad4f
caseyccc <51410580+caseyccc@users.noreply.github.com>
2019-12-18 14:50:56 -0500

    Update and rename setup to setup.bat

---
+++ setup.bat
@@ -0,0 +1,18 @@
+:: Install choco .exe and add choco to PATH
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+:: Install all the packages
+
+:::: Browsers
+choco install googlechrome -fy
+choco install firefox -fy
+
+::::Business
+choco install slack -fy
+choco install office365business -fy
+
+:::: Utilities + other
+choco install dropbox -fy
+choco install adobereader -fy
+choco install teamviewer  -fy
+choco install lastpass-chrome -fy
