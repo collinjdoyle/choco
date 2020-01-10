43b33850b46990613bedc2a11dbdf145330c9ee7
caseyccc <51410580+caseyccc@users.noreply.github.com>
2020-01-10 10:43:24 -0500

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,7 @@
 #Below is the following Powershell command to begin the "image". Add Programs as needed with the same syntax. 
 
-#$githubps = Invoke-WebRequest https://raw.githubusercontent.com/caseyccc/choco/master/setup.ps1 -UseBasicParsing
-#Invoke-Expression $($githubps.Content)
+#$githubps = iwr https://raw.githubusercontent.com/caseyccc/choco/master/setup.ps1 -UseBasicParsing
+#iex $($githubps.Content)
 
 #Install choco .exe and add choco to PATH
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
